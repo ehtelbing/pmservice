dbcf769611c76ba4958d18d23878589e296ea4d9
liangyinxue <lyx@newangels.cn>
2018-09-11 09:13:16 +0800

    lyx提交

---
+++ service/src/main/java/org/building/pmservice/service/soap/PmServiceImpl.java
@@ -0,0 +1,24 @@
+package org.building.pmservice.service.soap;
+
+import org.building.pmservice.service.repository.PmRepository;
+import org.building.pmservice.service.soap.PmService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.jws.WebService;
+import java.util.List;
+import java.util.Map;
+
+@WebService
+@Component
+public class PmServiceImpl implements PmService {
+
+    @Autowired
+    private PmRepository pmRepository;
+
+    @Override
+    public String getYearPlanList(String V_GUID) {
+        List<Map<String, Object>> list = pmRepository.getYearPlanList(V_GUID);
+        return "success";
+    }
+}
