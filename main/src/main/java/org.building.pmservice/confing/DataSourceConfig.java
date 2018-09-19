8155e7aa67e6c64dfcf63af07d49d78efa2f9266
张迹海 <zhangjihai@newangels.cn>
2018-09-19 17:18:42 +0800

    维修计划年计划接口

--- main/src/main/java/org.building.pmservice/confing/DataSourceConfig.java
+++ main/src/main/java/org.building.pmservice/confing/DataSourceConfig.java
@@ -8,15 +8,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
 
+import javax.annotation.Resource;
 import javax.sql.DataSource;
 
 @Configuration
-public class DataSourceConfig {
+public class DataSourceConfig implements TransactionManagementConfigurer {
+    @Resource
+    private PlatformTransactionManager transactionManager;
 
     @Primary
     @Bean(name = "oracleDatasource")
-    @ConfigurationProperties(prefix = "spring.datasource")
+    @ConfigurationProperties(prefix = "spring.oracle")
     public DataSource oracleDatasource() {
         return DataSourceBuilder.create().build();
     }
@@ -26,16 +32,33 @@ public class DataSourceConfig {
         return new NamedParameterJdbcTemplate(dataSource);
     }
 
-    @Bean(name = "secondDatasource")
-    @ConfigurationProperties(prefix = "spring.datasource.secondary")
+    @Bean(name = "sqlServiceDatasource")
+    @ConfigurationProperties(prefix = "spring.Sqlserver")
     public DataSource secondDataSource() {
         return DataSourceBuilder.create().build();
     }
 
-    @Bean(name = "secondJdbcTemplate")
-    public NamedParameterJdbcTemplate secondJdbcTemplate(@Qualifier("secondDatasource") DataSource dataSource) {
+    @Bean(name = "sqlServiceJdbcTemplate")
+    public NamedParameterJdbcTemplate sqlServiceJdbcTemplate(@Qualifier("sqlServiceDatasource") DataSource dataSource) {
         return new NamedParameterJdbcTemplate(dataSource);
     }
 
+    @Primary
+    @Bean(name = "oracleTransactionManager")
+    public DataSourceTransactionManager oracleTransactionManager(@Qualifier("oracleDatasource") DataSource dataSource) {
+        DataSourceTransactionManager transactionManagertemp = new DataSourceTransactionManager(dataSource);
+        transactionManager = transactionManagertemp;
+        return transactionManagertemp;
+    }
 
+    @Bean(name = "sqlServerTransactionManager")
+    public DataSourceTransactionManager mysqlTransactionManager(@Qualifier("sqlServiceDatasource") DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+    // 实现接口 TransactionManagementConfigurer 方法，其返回值代表在拥有多个事务管理器的情况下默认使用的事务管理器
+    @Override
+    public PlatformTransactionManager annotationDrivenTransactionManager() {
+        return transactionManager;
+    }
 }
--- service/src/main/java/org/building/pmservice/service/repository/PmRepository.java
+++ service/src/main/java/org/building/pmservice/service/repository/PmRepository.java
@@ -17,36 +17,38 @@ import java.util.*;
 public class PmRepository {
     @Autowired
     @Qualifier("oracleJdbcTemplate")
-    private NamedParameterJdbcTemplate oracleNameTemplate;
+    private NamedParameterJdbcTemplate oracleTemplate;
 
     @Autowired
-    @Qualifier("secondJdbcTemplate")
-    private NamedParameterJdbcTemplate sqlNameTemplate;
+    @Qualifier("sqlServiceJdbcTemplate")
+    private NamedParameterJdbcTemplate sqlServerTemplate;
 
 
-    @Transactional
+    @Transactional(value="sqlServerTransactionManager")
     public List<Map<String, Object>> getYearPlanList(String V_GUID){
 
         StringBuffer sql = new StringBuffer();
-        sql.append("select * from PM_03_PLAN_PROJECT where V_GUID =:v_guid");
+        sql.append("SELECT P.* ,O.V_WBS_NUM,R.V_WXJH_ORGGUID" +
+                " FROM pm_03_plan_project P " +
+                " LEFT JOIN BASE_DEPT O ON P.V_ORGCODE=O.V_DEPTCODE" +
+                " LEFT JOIN PM_03_PLAN_REPAIR_DEPT R ON P.V_GUID=R.V_GUID where P.V_GUID =:v_guid");
 
         Map<String, Object> query = new HashMap<String, Object>();
         query.put("v_guid", V_GUID);
 
-        List<Map<String, Object>> list = oracleNameTemplate.queryForList(sql.toString(), query);
+        List<Map<String, Object>> list = oracleTemplate.queryForList(sql.toString(), query);
 
         String v_year ="";
-        String v_orgcode="";
         String v_orgname = "";
         String v_project_code = "";
         String v_project_name = "";
-        String v_specialty = "";
+        UUID v_specialty = null;
         String v_specialtyman = "";
-        String v_wxtypecode = "";
-        String v_jhlb = "";
-        String v_sclb = "";
-        String v_cpzl = "";
-        String v_cpgx = "";
+        UUID v_wxtypecode = null;
+        UUID v_jhlb = null;
+        UUID v_sclb = null;
+        UUID v_cpzl = null;
+        UUID v_cpgx = null;
         String v_sgfs = "";
         String v_sfxj = "";
         String v_content = "";
@@ -56,6 +58,8 @@ public class PmRepository {
         String v_inman = "";
         String v_indate = "";
         String v_sumtime = "";
+        String V_WBS_NUM="";
+        UUID V_WXJH_ORGGUID=null;
 
         if(list!=null&&list.size()>0){
             try{
@@ -63,9 +67,7 @@ public class PmRepository {
                 if(list.get(0).get("V_YEAR")!=null&&!"".equals(list.get(0).get("V_YEAR"))){
                     v_year = list.get(0).get("V_YEAR").toString();//年份
                 }
-                if(list.get(0).get("V_ORGCODE")!=null&&!"".equals(list.get(0).get("V_ORGCODE"))){
-                    v_orgcode= list.get(0).get("V_ORGCODE").toString();//申请厂矿编码
-                }
+
                 if(list.get(0).get("V_ORGNAME")!=null&&!"".equals(list.get(0).get("V_ORGNAME"))){
                     v_orgname= list.get(0).get("V_ORGNAME").toString();//申请厂矿名称
                 }
@@ -76,25 +78,26 @@ public class PmRepository {
                     v_project_name = list.get(0).get("V_PORJECT_NAME").toString();//工程项目名称
                 }
                 if(list.get(0).get("V_SPECIALTY")!=null&&!"".equals(list.get(0).get("V_SPECIALTY"))){
-                    v_specialty = list.get(0).get("V_SPECIALTY").toString();//专业编码
+
+                    v_specialty = UUID.fromString(list.get(0).get("V_SPECIALTY").toString());//专业编码
                 }
                 if(list.get(0).get("V_SPECIALTYMAN")!=null&&!"".equals(list.get(0).get("V_SPECIALTYMAN"))){
                     v_specialtyman = list.get(0).get("V_SPECIALTYMAN").toString();//专业负责人姓名
                 }
                 if(list.get(0).get("V_WXTYPECODE")!=null&&!"".equals(list.get(0).get("V_WXTYPECODE"))){
-                    v_wxtypecode = list.get(0).get("V_WXTYPECODE").toString();//维修类型编码
+                    v_wxtypecode = UUID.fromString(list.get(0).get("V_WXTYPECODE").toString());//维修类型编码
                 }
                 if(list.get(0).get("V_JHLB")!=null&&!"".equals(list.get(0).get("V_JHLB"))){
-                    v_jhlb = list.get(0).get("V_JHLB").toString();//计划类别
+                    v_jhlb = UUID.fromString(list.get(0).get("V_JHLB").toString());//计划类别
                 }
                 if(list.get(0).get("V_SCLB")!=null&&!"".equals(list.get(0).get("V_SCLB"))){
-                    v_sclb = list.get(0).get("V_SCLB").toString();//生产类别
+                    v_sclb = UUID.fromString(list.get(0).get("V_SCLB").toString());//生产类别
                 }
                 if(list.get(0).get("V_CPZL")!=null&&!"".equals(list.get(0).get("V_CPZL"))){
-                    v_cpzl = list.get(0).get("V_CPZL").toString();//产品种类
+                    v_cpzl = UUID.fromString(list.get(0).get("V_CPZL").toString());//产品种类
                 }
                 if(list.get(0).get("V_CPGX")!=null&&!"".equals(list.get(0).get("V_CPGX"))){
-                    v_cpgx = list.get(0).get("V_CPGX").toString();//产品工序
+                    v_cpgx = UUID.fromString(list.get(0).get("V_CPGX").toString());//产品工序
                 }
                 if(list.get(0).get("V_SGFS")!=null&&!"".equals(list.get(0).get("V_SGFS"))){
                     v_sgfs = list.get(0).get("V_SGFS").toString();//施工方式
@@ -123,26 +126,32 @@ public class PmRepository {
                 if(list.get(0).get("V_SUMTIME")!=null&&!"".equals(list.get(0).get("V_SUMTIME"))){
                     v_sumtime = list.get(0).get("V_SUMTIME").toString();//合计工时
                 }
+                if(list.get(0).get("V_WBS_NUM")!=null&&!"".equals(list.get(0).get("V_WBS_NUM"))){
+                    V_WBS_NUM = list.get(0).get("V_WBS_NUM").toString();//单位序号
+                }
+
+                if(list.get(0).get("V_WXJH_ORGGUID")!=null&&!"".equals(list.get(0).get("V_WXJH_ORGGUID"))){
+                    V_WXJH_ORGGUID = UUID.fromString(list.get(0).get("V_WXJH_ORGGUID").toString());//检修单位
+                }
 
                 //插入sqlserver数据库
                 String insertSql = "insert into dbo.akwx_njh(ckmc,ckxh,jhnf,gcmc,gcbh,zybm,gcnr,tzzj," +
                         "sgfsbm,sfxj,wxlxbm,jhlbbm,gxbm,sclbbm,cpzlbm,yjkgsj,yjjgsj," +
-                        "yjgq,fzr,qcrxm,qcrq,xmzt,yjhsy) " +
+                        "yjgq,fzr,qcrxm,qcrq,xmzt,yjhsy,jhxz,zcyz,qcrbm,qcrjs,dqhj,bjf,clf,cxf,wwf,xjxf,sjjlf,qtf) " +
                         "values(:v_orgname,:v_orgcode,:v_year,:v_project_name,:v_project_code,:v_specialty,:v_content,:v_moneybudget," +
                         ":v_sgfs,:v_sfxj,:v_wxtypecode,:v_jhlb,:v_cpgx,:v_sclb,:v_cpzl,:v_bdate,:v_edate," +
-                        ":v_sumtime,:v_specialtyman,:v_inman,:v_indate,:v_xmzt,:v_yjhsy)";
-
+                        ":v_sumtime,:v_specialtyman,:v_inman,:v_indate,:v_xmzt,:v_yjhsy,:v_jhxz,:v_zcyz,:v_qcrbm,:v_qcrjs,:v_dqhj,:v_bjf,:v_clf,:v_cxf,:v_wwf,:v_xjxf,:v_sjjlf,:v_qtf)";
                 Map<String, Object> insertMap = new HashMap<String, Object>();
                 SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
                 SimpleDateFormat sdfDate = new SimpleDateFormat("yyyy-MM-dd");
                 insertMap.put("v_orgname", v_orgname);
-                insertMap.put("v_orgcode", v_orgcode);
+                insertMap.put("v_orgcode", V_WBS_NUM);
                 insertMap.put("v_year", v_year);
                 insertMap.put("v_project_name", v_project_name);
                 insertMap.put("v_project_code", v_project_code);
                 insertMap.put("v_specialty", v_specialty);
                 insertMap.put("v_content", v_content);
-                insertMap.put("v_moneybudget", Double.valueOf(v_moneybudget));
+                insertMap.put("v_moneybudget", v_moneybudget);
 
                 insertMap.put("v_sgfs", v_sgfs);
                 insertMap.put("v_sfxj", v_sfxj);
@@ -151,6 +160,15 @@ public class PmRepository {
                 insertMap.put("v_cpgx",v_cpgx);
                 insertMap.put("v_sclb",v_sclb);
                 insertMap.put("v_cpzl",v_cpzl);
+
+                insertMap.put("v_bjf",0);
+                insertMap.put("v_clf",0);
+                insertMap.put("v_cxf",0);
+                insertMap.put("v_wwf",v_moneybudget);
+                insertMap.put("v_xjxf",0);
+                insertMap.put("v_sjjlf",0);
+                insertMap.put("v_qtf",0);
+
                 if(v_bdate!=null&&!"".equals(v_bdate)){
                     v_bdate = v_bdate.substring(0,10);
                 }
@@ -162,14 +180,22 @@ public class PmRepository {
 
 
                 insertMap.put("v_sumtime",v_sumtime);
-                insertMap.put("v_specialtyman",v_specialtyman);
-                insertMap.put("v_inman",v_inman);
+                insertMap.put("v_specialtyman","厂矿计划员");
+                insertMap.put("v_inman","厂矿计划员");
                 insertMap.put("v_indate",v_indate);
-                insertMap.put("v_xmzt",0);
+                insertMap.put("v_xmzt",1);
                 insertMap.put("v_yjhsy",0);
 
+                insertMap.put("v_jhxz",0);
+                insertMap.put("v_zcyz",2000);
+                //insertMap.put("v_tjdw1",V_WXJH_ORGGUID);
+                UUID v_qcrbm=UUID.fromString("A0C4A846-5FF9-4609-8173-87328978AEC4");
+                insertMap.put("v_qcrbm",v_qcrbm);
+                insertMap.put("v_qcrjs","厂矿计划员");
+                insertMap.put("v_dqhj",0);
 
-                sqlNameTemplate.update(insertSql,insertMap);
+
+                sqlServerTemplate.update(insertSql,insertMap);
 
             }catch (Exception e){
                 e.printStackTrace();
